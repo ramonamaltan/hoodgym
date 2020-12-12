14c75f544a0a772511136a3720c8f0e406ee216e
Ramona Maltan <ramonamaltan@gmail.com>
2020-12-12 12:16:34 +0100

    stations migration for lat and long, added markers to controller and map to view

--- app/models/station.rb
+++ app/models/station.rb
@@ -5,5 +5,5 @@ class Station < ApplicationRecord
   validates :location, uniqueness: true
 
   geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_location?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_100937) do
+ActiveRecord::Schema.define(version: 2020_12_12_111059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2020_12_11_100937) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
