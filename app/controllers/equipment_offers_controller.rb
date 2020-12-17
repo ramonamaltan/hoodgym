d8aa61e82c7d04fbb1f81bc256fbd075b272fb64
Ramona Maltan <ramonamaltan@gmail.com>
2020-12-17 23:06:48 +0100

    added all page content

--- app/controllers/equipment_offers_controller.rb
+++ app/controllers/equipment_offers_controller.rb
@@ -1,5 +1,7 @@
 class EquipmentOffersController < ApplicationController
   def index
-    @equipment_offers = policy_scope(EquipmentOffer).order(created_at: :desc)
+    @station = Station.find(params[:station_id])
+    all_offers = policy_scope(EquipmentOffer).order(created_at: :desc)
+    @equipment_offers = all_offers.where(station_id: @station)
   end
 end
