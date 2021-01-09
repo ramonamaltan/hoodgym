a55e98793e89b1fcd758eee2efda73bc83a5a2b4
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-09 18:05:36 +0100

    solving showing background image on home in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,12 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
+
+  # added to make background image visible
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
