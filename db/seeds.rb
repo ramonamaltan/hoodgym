d8b3773a426129385ce0f736beed585f642d090d
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-14 18:34:26 +0100

    shortened station name from scraper

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,18 @@ Station.destroy_all
 # Webscraping for stations
 puts 'Start scraping'
 
-html_content = open('https://calisthenics-parks.com/cities/16-de-berlin').read
+html_content = open('https://calisthenics-parks.com/cities/16-en-berlin').read
 doc = Nokogiri::HTML(html_content)
 
 nodeset = doc.css(".media-heading a")
 links = nodeset.map {|element| element["href"] }.compact
 nodeset.each do |station|
-  name = station.children.to_s
+  p station.children.to_s.gsub("- Berlin ", "").gsub("Berlin - ", "").gsub('Calisthenics Stations - ', '').gsub('Ninja Warrior Gym - ', '')
   link = station["href"]
   station_url = open(link).read
   station_doc = Nokogiri::HTML(station_url)
   address = station_doc.search('address').text.strip
-  location = address.gsub("Adresse\n                                    ", "").gsub(", Deutschland", "")
+  p location = address.gsub("Address\n                                    ", "").gsub(", Germany", "")
   Station.create(name: name, location: location)
 end
 
--- db/station-scraper.rb
+++ db/station-scraper.rb
@@ -1,17 +1,17 @@
 require 'open-uri'
 require 'nokogiri'
 
-html_content = open('https://calisthenics-parks.com/cities/16-de-berlin').read
+html_content = open('https://calisthenics-parks.com/cities/16-en-berlin').read
 doc = Nokogiri::HTML(html_content)
 
 nodeset = doc.css(".media-heading a")
 links = nodeset.map {|element| element["href"] }.compact
 nodeset.each do |station|
-  p station.children.to_s
+  p station.children.to_s.gsub("- Berlin ", "").gsub("Berlin - ", "").gsub('Calisthenics Stations - ', '').gsub('Ninja Warrior Gym - ', '')
   link = station["href"]
   station_url = open(link).read
   station_doc = Nokogiri::HTML(station_url)
   address = station_doc.search('address').text.strip
-  p location = address.gsub("Adresse\n                                    ", "").gsub(", Deutschland", "")
+  p location = address.gsub("Address\n                                    ", "").gsub(", Germany", "")
 end
 
