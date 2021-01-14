db6fe7b6e3c7b6bf02b383c7e371bfef8755e15a
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-14 17:44:52 +0100

    generated 50 stations with webscraper

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,24 @@ EquipmentOffer.destroy_all
 Station.destroy_all
 
 # Webscraping for stations
+puts 'Start scraping'
+
 html_content = open('https://calisthenics-parks.com/cities/16-de-berlin').read
 doc = Nokogiri::HTML(html_content)
 
 nodeset = doc.css(".media-heading a")
 links = nodeset.map {|element| element["href"] }.compact
 nodeset.each do |station|
-  p station.children.to_s
+  name = station.children.to_s
   link = station["href"]
   station_url = open(link).read
   station_doc = Nokogiri::HTML(station_url)
   address = station_doc.search('address').text.strip
-  p location = address.gsub("Adresse\n                                    ", "").gsub(", Deutschland", "")
+  location = address.gsub("Adresse\n                                    ", "").gsub(", Deutschland", "")
+  Station.create(name: name, location: location)
 end
+
+puts "finished scraping & created #{Station.count} stations"
 
 # Rest of seeds
 puts 'Start seeding users'
