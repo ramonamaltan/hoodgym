b447579056657e04da9dc946be76f07c8f7412ad
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-25 12:31:53 +0100

    comments for how to create and finish rent

--- app/controllers/equipment_offers_controller.rb
+++ app/controllers/equipment_offers_controller.rb
@@ -7,5 +7,13 @@ class EquipmentOffersController < ApplicationController
     @current_user = current_user
     # Rent in mins --> renttime = (rental1.end_at - rental1.begin_at)/60
     @myrentals = Rental.where(user_id: current_user).where(end_at: nil)
+
+    # Create a rental
+    # Rental.new
+    # begin_at: DateTime.now().utc
+    # user_id = current_user
+    # equipment_id = Pick up dumbbell to start rent 
+
+    # Finish rental on return --> set DateTime.now().utc
   end
 end
