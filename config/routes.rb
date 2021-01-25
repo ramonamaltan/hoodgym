16d1abad98208ee507bcb4fe02cf3d2f93e99fc4
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-25 10:25:21 +0100

    show all bookings of current usere

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/pricing', to: 'pages#pricing'
+  get '/bookings', to: 'rentals#booking'
   resources :stations, only: [:index, :show] do 
     get '/training', to: 'equipment_offers#training', as: 'training'
   end
