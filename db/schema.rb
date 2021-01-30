18e966bb468e936034c90058baa89d70b668424d
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-30 11:20:30 +0100

    added admin column to user default false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_090202) do
+ActiveRecord::Schema.define(version: 2021_01_30_101757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_12_13_090202) do
     t.string "last_name"
     t.integer "time_account"
     t.string "location"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
