e80ffee1e4490838db021fb113daee131a50190d
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-30 12:16:42 +0100

    station creation and image upload working

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -30,6 +30,7 @@ class StationsController < ApplicationController
 
   def create
     @station = Station.new(station_params)
+    authorize @station
     if @station.save
       redirect_to station_path(@station)
     else
--- app/models/station.rb
+++ app/models/station.rb
@@ -7,5 +7,5 @@ class Station < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 
-  has_many_attached :photos
+  has_one_attached :photo
 end
