b3bb2be1f638ed04d48504e989f78a00e92b823f
Ramona Maltan <ramonamaltan@gmail.com>
2021-02-28 10:40:57 +0100

    chatroom show action and view

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   end
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/pricing', to: 'pages#pricing'
   get '/bookings', to: 'rentals#booking'
+  resources :chatrooms, only: [:show]
   resources :stations, only: [:index, :show, :new, :create] do 
     get '/training', to: 'equipment_offers#training', as: 'training'
   end
